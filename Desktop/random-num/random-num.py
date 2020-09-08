41eb3fd7f53a3ea7a278507ba6a54ac2de921710
Eumenes Mau <ppqqla6248@hotmail.com>
2020-09-08 16:06:52 +0800

    final2

--- Desktop/random-num/random-num.py
+++ Desktop/random-num/random-num.py
@@ -9,18 +9,20 @@ count=0
 while True:
 	start = str(start)
 	end = str(end)
-	guess = input('猜數字遊戲喔~請從' + start + '-'+ end +'以內猜出正確數字:')
+	guess = input('猜數字遊戲喔~請從' + start + '-'+ end + '以內猜出正確數字:')
 	guess = int(guess)
 	count += 1
 	print('你目前猜了', count, '次了')
+	start = int(start)
+	end = int(end)
 	if guess == num:
 		print('這你也猜得對，太屌了吧!')
 		break
-	elif guess > num and guess < 101 and guess > 0:
+	elif guess > num and guess <= end and guess >= start:
 		print('答案比你猜的', guess, '還要小')
-	elif guess < num and guess < 101 and guess > 0:
+	elif guess < num and guess <= end and guess >= start:
 		print('答案比你猜的', guess, '還要大')
-	#elif guess > 100 and guess < 1:
-	#	print('請從1-100以內去猜喔!')
 	else:
-		print('請從1-100填入數字喔!')
+		start = str(start)
+		end = str(end)
+		print('請從' + start + '-'+ end + '填入數字喔!')
